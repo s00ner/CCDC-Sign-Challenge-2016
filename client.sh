cc0f0911fbb596ab6b4743476010fa3c3460879f
s00ner <s00ner@doublehack.me>
2016-04-02 13:11:46 +0000

    changed comments

--- client.sh
+++ client.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-#usage: ./client.sh [key] [message]
-#example: ./client.sh 4e9f "This is my message"
+#usage: ./client.sh [message]
+#example: ./client.sh "This is my message"
 #message is the message to be sent to the sign
-#key is the 4 character hex key. It changes once in awhile so don't forget to check the key file for it.
 #this script sets the message timer to 30 minutes so the message will be deleted from the sign after 30 minutes has passed
 
 message=\"$1\"
